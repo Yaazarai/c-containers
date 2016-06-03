099146c52d8f7be121926a093d948e9ae0471c6a
Alexander Griffis <fatalsleep@gmail.com>
2016-06-03 16:19:18 -0700

    Additional functions.

--- containers/cstack.h
+++ containers/cstack.h
@@ -14,15 +14,20 @@
     #define cs_cache(ptr) cb_cacheg(ptr)
     #define cs_resize(ptr, length) cb_resizeg(ptr, length)
 
+    #define cs_clear(ptr) cb_clearg(ptr)
+    #define cs_empty(ptr) cb_emptyg(ptr)
+
+    #define cs_size(ptr) cb_sizeg(ptr)
+    #define cs_cachesize(ptr) cb_cachesizeg(ptr)
+    #define cs_maxsize(ptr) cb_maxsizeg(ptr)
+    #define cs_count(ptr) cb_countg(ptr)
+
+    #define cs_begin(ptr) cb_beging(ptr)
+    #define cs_end(ptr) cb_endg(ptr)
+    #define cs_rbegin(ptr) cb_rbeging(ptr)
+    #define cs_rend(ptr) cb_rendg(ptr)
+
     type cs_top(cstack* cstk);
     void cs_push(cstack* cstk, type data);
     void cs_pop(cstack* cstk);
-
-    void cs_swap(cstack* cstks, cstack* cstkd);
-    cbool cs_empty(cstack* cstk);
-
-    ui32 cs_size(cstack* cstk);
-    ui32 cs_cachesize(cstack* cstk);
-    ui32 cs_maxsize(cstack* cstk);
-    ui32 cs_count(cstack* cstk);
 #endif
