e516a1e78cdf429e8b0bf08d6c14492983289ce0
Alexander Griffis <fatalsleep@gmail.com>
2016-06-03 16:39:04 -0700

    Fixed not zeroing memory on pop.

--- containers/cstack.c
+++ containers/cstack.c
@@ -6,17 +6,19 @@ type cs_top(cstack* cstk) {
 };
 
 void cs_push(cstack* cstk, type data) {
-    if (cstk->cached <= 0)
+    if (cstk->cached == 0)
         cb_cache((cbase*) cstk);
 
     memcpy(cstk->memory + (cstk->length * cstk->typesz), data, cstk->typesz);
-    
+
     cstk->length ++;
     cstk->cached --;
 };
 
 void cs_pop(cstack* cstk) {
     if (cstk->length > 0) {
+        memset(cstk->memory + ((cstk->length - 1) * cstk->typesz), 0, cstk->typesz);
+
         cstk->length --;
         cstk->cached ++;
     }
