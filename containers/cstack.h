81e741d6b2e0722a3fa43ac1a2e9c185890a462b
Alexander Griffis <fatalsleep@gmail.com>
2016-06-03 08:00:57 -0700

    Fixed missing argument in macro.

--- containers/cstack.h
+++ containers/cstack.h
@@ -9,7 +9,7 @@
         ui08* memory;
     } cstack;
 
-    #define cs_alloc(ty, cachesz) cb_allocg(ty, cachesz)
+    #define cs_alloc(strc, ty, cachesz) cb_allocg(strc, ty, cachesz)
     #define cs_free(ptr) cb_freeg(ptr)
     #define cs_cache(ptr) cb_cacheg(ptr)
     #define cs_resize(ptr, length) cb_resizeg(ptr, length)
