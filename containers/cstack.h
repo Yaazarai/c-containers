04b091cf843e40beb8833097773927fce823a6c4
Alexander Griffis <fatalsleep@gmail.com>
2016-06-03 23:03:25 -0700

    Minor API changes.

--- containers/cstack.h
+++ containers/cstack.h
@@ -2,14 +2,14 @@
 #define C_STACK
     #include <stdlib.h>
     #include <string.h>
-    #include "base\cbase.h"
+    #include "cbase.h"
 
     typedef struct c_stack {
         ui32 typesz, cachesz, length, cached;
         ui08* memory;
     } cstack;
 
-    #define cs_alloc(strc, ty, cachesz) cb_allocg(strc, ty, cachesz)
+    #define cs_alloc(ty, cachesz) cb_allocg(cstack, ty, cachesz)
     #define cs_free(ptr) cb_freeg(ptr)
     #define cs_cache(ptr) cb_cacheg(ptr)
     #define cs_resize(ptr, length) cb_resizeg(ptr, length)
